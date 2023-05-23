66172b061cfd75e0f77234bc7309b20035273e8c
GaBen <galen.brotherton@student.heaj.be>
2023-05-23 13:38:52 +0200

    Premiere début de page Dalto+la nav en responsive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,40 @@
+'use strict';
+
 console.info('Hello world');
+
+
+const navSlide = () => {
+    const burgerm = document.querySelector('.burgerm');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const body = document.querySelector(".body");
+  
+    burgerm.addEventListener('click', () => {
+     
+      nav.classList.toggle('nav-active');
+      body.classList.toggle('scroll');
+     
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = '';
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+        }
+      });
+      
+      burgerm.classList.toggle('toggle')
+    });
+  };
+  
+  const app = () => {
+    navSlide();
+  };
+  
+  app();
+
+ 
+
+const themeTrigger = document.querySelector(".js-theme-trigger");
+if(themeTrigger){
+    themeTrigger.addEventListener("click", toggleTheme) ;
+}
